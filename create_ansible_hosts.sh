e64ad94dbffa3b2aae7cc1dc6bcc1951b4683942
Abdallah Refay <abdallah.refay1999@gmail.com>
2022-05-20 02:53:26 +0200

    edit create_ansible_hosts.sh

--- create_ansible_hosts.sh
+++ create_ansible_hosts.sh
@@ -7,11 +7,11 @@ $1
 $2
 
 [private:vars]
-ansible_ssh_private_key_file=${JENKINS_HOME}/workspace/terraform/private_key.pem
+ansible_ssh_private_key_file=${JENKINS_HOME}/workspace/terraform/provisioner/private_key.pem
 ansible_user=ubuntu
-ansible_ssh_common_args='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p -q ubuntu@$3 -o IdentityFile=~/terraform/provisioner/private_key.pem -o Port=22"'
+ansible_ssh_common_args='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p -q ubuntu@$3 -o IdentityFile=${JENKINS_HOME}/workspace/terraform/provisioner/private_key.pem -o Port=22"'
 
 [public:vars]
-ansible_ssh_private_key_file=${JENKINS_HOME}/workspace/terraform/private_key.pem
+ansible_ssh_private_key_file=${JENKINS_HOME}/workspace/terraform/provisioner/private_key.pem
 ansible_user=ubuntu
 EOF
