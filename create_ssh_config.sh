3a984cb9c2195429c4cc1ae2352483e6158d1530
Abdallah Refay <abdallah.refay1999@gmail.com>
2022-05-20 23:45:02 +0200

    edit create_ssh_config.sh

--- create_ssh_config.sh
+++ create_ssh_config.sh
@@ -1,9 +1,11 @@
 tee ~/.ssh/config <<EOF
 Host $1
+  User ubuntu
   ProxyCommand ssh -W %h:%p $3
   IdentityFile ${JENKINS_HOME}/workspace/terraform/provisioner/private_key.pem
 
 Host $2
+  User ubuntu
   ProxyCommand ssh -W %h:%p $3
   IdentityFile ${JENKINS_HOME}/workspace/terraform/provisioner/private_key.pem
 
