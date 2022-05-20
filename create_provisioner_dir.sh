ca26d0e7844ea02399fa949e156108a28eb47cd4
Abdallah Refay <abdallah.refay1999@gmail.com>
2022-05-20 02:19:37 +0200

    test build

--- create_provisioner_dir.sh
+++ create_provisioner_dir.sh
@@ -2,7 +2,6 @@
 #you could use it to output any secret data it's added to the gitignore file
 #it should be created because it would through an error if it doesn't exist 
 #you can run the following script and you will be good to go
-
 if [ -d ./provisioner ]
     then
     echo "provisioner directory already exists all good"
