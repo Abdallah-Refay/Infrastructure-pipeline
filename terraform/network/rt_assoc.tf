2b9f61c3628ba7c41cd6e508dac890992616af41
Abdallah Refaay <abdallah.refay1999@gmail.com>
2022-06-10 05:03:40 +0200

    restored public subnet2

--- terraform/network/rt_assoc.tf
+++ terraform/network/rt_assoc.tf
@@ -3,10 +3,10 @@ resource "aws_route_table_association" "public1" {
   route_table_id = aws_route_table.public_route.id
 }
 
-# resource "aws_route_table_association" "public2" {
-#   subnet_id      = aws_subnet.tf_public_subnet2.id
-#   route_table_id = aws_route_table.public_route.id
-# }
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.tf_public_subnet2.id
+  route_table_id = aws_route_table.public_route.id
+}
 
 resource "aws_route_table_association" "private1" {
   subnet_id      = aws_subnet.tf_private_subnet1.id
